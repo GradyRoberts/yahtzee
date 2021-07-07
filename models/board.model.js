eccdbc38fd42a5310f7176cd80a16cb40033af3f
Grady Roberts <gnr7aj@virginia.edu>
2021-07-06 21:41:01 -0400

    update models

--- models/board.model.js
+++ models/board.model.js
@@ -1,26 +1,69 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-	sequelize.define('board', {
-		playerID: {
+	sequelize.define('Board', {
+		playerId: {
 			type: DataTypes.STRING,
 			allowNull: false,
+			unique: true,
 			primaryKey: true,
 		},
-		ones: DataTypes.INTEGER,
-		twos: DataTypes.INTEGER,
-		threes: DataTypes.INTEGER,
-		fours: DataTypes.INTEGER,
-		fives: DataTypes.INTEGER,
-		sixes: DataTypes.INTEGER,
-		threeOAK: DataTypes.INTEGER,
-		fourOAK: DataTypes.INTEGER,
-		fullHouse: DataTypes.INTEGER,
-		smStraight: DataTypes.INTEGER,
-		lgStraight: DataTypes.INTEGER,
-		yahtzee: DataTypes.INTEGER,
-		chance: DataTypes.INTEGER,
-		yahtzeeBonusCnt: DataTypes.INTEGER,
+		ones: {
+			type: DataTypes.INTEGER,
+			defaultValue: -1,
+		},
+		twos: {
+			type: DataTypes.INTEGER,
+			defaultValue: -1,
+		},
+		threes: {
+			type: DataTypes.INTEGER,
+			defaultValue: -1,
+		},
+		fours: {
+			type: DataTypes.INTEGER,
+			defaultValue: -1,
+		},
+		fives: {
+			type: DataTypes.INTEGER,
+			defaultValue: -1,
+		},
+		sixes: {
+			type: DataTypes.INTEGER,
+			defaultValue: -1,
+		},
+		threeOAK: {
+			type: DataTypes.INTEGER,
+			defaultValue: -1,
+		},
+		fourOAK: {
+			type: DataTypes.INTEGER,
+			defaultValue: -1,
+		},
+		fullHouse: {
+			type: DataTypes.INTEGER,
+			defaultValue: -1,
+		},
+		smStraight: {
+			type: DataTypes.INTEGER,
+			defaultValue: -1,
+		},
+		lgStraight: {
+			type: DataTypes.INTEGER,
+			defaultValue: -1,
+		},
+		yahtzee: {
+			type: DataTypes.INTEGER,
+			defaultValue: -1,
+		},
+		chance: {
+			type: DataTypes.INTEGER,
+			defaultValue: -1,
+		},
+		yahtzeeBonusCnt: {
+			type: DataTypes.INTEGER,
+			defaultValue: 0,
+		},
 	}, {
 		tableName: 'Boards',
 	});
--- models/game.model.js
+++ models/game.model.js
@@ -1,21 +1,34 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-	sequelize.define('game', {
-		gameID: {
-			type: DataTypes.INTEGER,
-			unique: true,
-			autoIncrement: true,
-			primaryKey: true,
-		},
+	sequelize.define('Game', {
+		// gameID: {
+		// 	type: DataTypes.INTEGER,
+		// 	unique: true,
+		// 	autoIncrement: true,
+		// 	primaryKey: true,
+		// },
 		player1: {
 			type: DataTypes.STRING,
+			unique: true,
 			allowNull: false,
 		},
-		player2: DataTypes.STRING,
-		player3: DataTypes.STRING,
-		player4: DataTypes.STRING,
-		player5: DataTypes.STRING,
+		player2: {
+			type: DataTypes.STRING,
+			unique: true,
+		},
+		player3: {
+			type: DataTypes.STRING,
+			unique: true,
+		},
+		player4: {
+			type: DataTypes.STRING,
+			unique: true,
+		},
+		player5: {
+			type: DataTypes.STRING,
+			unique: true,
+		},
 		round: {
 			type: DataTypes.INTEGER,
 			allowNull: false,
@@ -23,6 +36,34 @@ module.exports = (sequelize) => {
 			validate: {
 				min: 1,
 				max: 13,
+			},
+		},
+		lastRollValue: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			defaultValue: '6-6-6-6-6',
+		},
+		lastRollImg: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			defaultValue: '../images/base.png',
+		},
+		currentPlayer: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			defaultValue: 1,
+			validate: {
+				min: 1,
+				max: 5,
+			},
+		},
+		currentRoll: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			defaultValue: 1,
+			validate: {
+				min: 1,
+				max: 3,
 			},
 		},
 	}, {
